2d340391fdd873dac666876de1d944fb1918b2f9
Ivan Lukyanau <51121952+Lukyanau@users.noreply.github.com>
2021-02-03 16:13:09 +0300

    Update ServiceImplTest.java

--- Task4Git/src/test/java/by/epam_training/lukyanov/task4/serviceImpl/ServiceImplTest.java
+++ Task4Git/src/test/java/by/epam_training/lukyanov/task4/serviceImpl/ServiceImplTest.java
@@ -11,21 +11,50 @@ import static org.junit.Assert.*;
 public class ServiceImplTest {
     ServiceImpl service;
     TextParser textParser;
+    
     @Before
     public void setUp() throws Exception {
-       service = ServiceImpl.getInstance();
-       textParser = TextParser.getInstance();
+        service = ServiceImpl.getInstance();
+        textParser = TextParser.getInstance();
     }
 
     @Test
     public void printAllShouldReturnCorrectText() throws ProjectException {
-        String allText = "Parsed Text:\n"+
+        String expectedText = "Parsed Text:\n" +
                 "We can have different type of houses, such as Wooden House and Glass House.\n" +
                 "Hi hi.\n" +
                 "House.\n" +
-                "Here is the code.\n"+
+                "Here is the code.\n" +
                 "We could have overridden other methods also, but for simplicity I am not doing that.";
-        String expectedText = service.printAllText(textParser.generateParsedTest("resources/programmingText.txt"));
-        assertSame(allText, expectedText.trim());
+        String actualText = service.printAllText(textParser.generateParsedTest("resources/programmingText.txt"));
+        assertEquals(expectedText, actualText.trim());
+    }
+
+    @Test
+    public void findRepeatingWordShouldReturnCorrectObject() throws ProjectException {
+        List<String> expectedWord = new ArrayList<String>
+                (Arrays.asList("We can have different type of houses, such as Wooden House and Glass House.", "Hi hi."));
+        List<String> actualWord = service.findRepeatingWords(textParser.generateParsedTest
+                ("resources/programmingText.txt")).getSentences();
+        assertEquals(expectedWord, actualWord);
+    }
+
+    @Test
+    public void sortSentenceByNumberOfWordsShouldReturnCorrectObject() throws ProjectException {
+        List<String> expectedWord = new ArrayList<String>
+                (Arrays.asList("House.","Hi hi.", "Here is the code.", "We can have different type of houses, such as Wooden House and Glass House.",
+                        "We could have overridden other methods also, but for simplicity I am not doing that."));
+        List<String> actualWord = service.sortSentenceByNumberOfWords(textParser.generateParsedTest
+                ("resources/programmingText.txt")).getSentences();
+        assertEquals(expectedWord, actualWord);
+    }
+
+    @Test
+    public void findUniqueWordInFirstSentenceShouldReturnCorrectObject() throws ProjectException {
+        List<String> expectedWord = new ArrayList<String>
+                (Arrays.asList("can", "different", "type", "of", "houses", "such", "as", "Wooden", "and", "Glass"));
+        List<String> actualWord = service.findUniqueWordsInFirstSentence(textParser.generateParsedTest
+                ("resources/programmingText.txt")).getSentences();
+        assertEquals(expectedWord, actualWord);
     }
 }
